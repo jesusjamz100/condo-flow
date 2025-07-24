5a87a28b496738dbfed4d1c86a02d5eb87136d42
jesusjamz100 <jesusjamz100@gmail.com>
2025-07-24 13:40:29 -0400

    created basic entities and relations for Resident, Apartment and ParkingLot

---
+++ services/condo/src/main/java/com/condoflow/condo/apartment/parking/ParkingSlot.java
@@ -0,0 +1,30 @@
+package com.condoflow.condo.apartment.parking;
+
+import com.condoflow.condo.apartment.Apartment;
+import com.condoflow.condo.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import static jakarta.persistence.EnumType.STRING;
+
+@Entity
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class ParkingSlot extends BaseEntity {
+
+    private Integer slotNumber;
+
+    @Enumerated(STRING)
+    private Location location;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "apartment_id")
+    private Apartment apartment;
+}
