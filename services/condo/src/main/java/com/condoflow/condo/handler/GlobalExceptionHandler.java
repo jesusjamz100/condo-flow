0ae1c2d04d68533db4b85c3027deb36825f30178
jesusjamz100 <jesusjamz100@gmail.com>
2025-07-27 12:25:19 -0400

    added a handler for access denied exception

--- services/condo/src/main/java/com/condoflow/condo/handler/GlobalExceptionHandler.java
+++ services/condo/src/main/java/com/condoflow/condo/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.condoflow.condo.exception.EmailAlreadyUsedException;
 import com.condoflow.condo.exception.ResidentAlreadyExistsException;
 import com.condoflow.condo.exception.ResidentNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -59,6 +60,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(BAD_REQUEST)
                 .body(new ErrorResponse(errors));
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<Void> handleException(AuthorizationDeniedException exp) {
+        return ResponseEntity
+                .status(UNAUTHORIZED)
+                .build();
     }
 
     @ExceptionHandler(Exception.class)
