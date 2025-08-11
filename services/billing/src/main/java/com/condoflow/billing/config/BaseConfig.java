2b0f9a59581a167e0c7460abaeeded35baec4a99
jesusjamz100 <jesusjamz100@gmail.com>
2025-08-11 19:04:58 -0400

    initial setup

---
+++ services/billing/src/main/java/com/condoflow/billing/config/BaseConfig.java
@@ -0,0 +1,11 @@
+package com.condoflow.billing.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+@EnableDiscoveryClient
+public class BaseConfig {
+}
